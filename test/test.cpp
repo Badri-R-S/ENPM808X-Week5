56984f975fb56887e137e22ac114bf9e632a0104
Badrinarayanan <badrinarayanan008@gmail.com>
2022-10-03 21:57:16 -0400

    Updated for lcov generation

--- test/test.cpp
+++ test/test.cpp
@@ -15,29 +15,29 @@ TEST(testingforkp, Testfail)
 {
     PID pid(2, 1, 0.5, 0.05, 10, -10);
     std::cout << pid.getKp() << "_____" << Kpmax << "_____" << Kpmin;
-    EXPECT_FALSE(IsBetweenmaxmin(pid.getKp(), Kpmin, Kpmax));
+    EXPECT_TRUE(IsBetweenmaxmin(pid.getKp(), Kpmin, Kpmax));
 }
  /// Unit test to check if the Integral constant is in the range
 TEST(testingforki, TestFail)
  {
      PID pid(1, 2, 0.5, 0.05, 10, -10);
-     EXPECT_FALSE(IsBetweenmaxmin(pid.getKi(), Kimin, Kimax));
+     EXPECT_TRUE(IsBetweenmaxmin(pid.getKi(), Kimin, Kimax));
  }
  /// Unit test to check if the Derivative constant is in the range
  TEST(testingforkd, TestFail)
  {
      PID pid(1, 1, 2, 0.05, 10, -10);
-     EXPECT_FALSE(IsBetweenmaxmin(pid.getKd(), Kdmin, Kdmax));
+     EXPECT_TRUE(IsBetweenmaxmin(pid.getKd(), Kdmin, Kdmax));
  }
  /// Unit test to check if dt is in the range
  TEST(testingfortime, TestFail)
  {
      PID pid(1, 1, 0.5, 0.5, 10, -10);
-     EXPECT_FALSE(IsBetweenmaxmin(pid.getdt(), 0, tmax));
+     EXPECT_TRUE(IsBetweenmaxmin(pid.getdt(), 0, tmax));
  }
 /// Unit test to check compute function
 TEST(Pidcompute, testcompute) {
     PID pid(1, 1, 0.5, 0.5, 10, -10);
     double result = pid.compute(0.1, 0.2);
-    ASSERT_EQ(result, 5);
+    ASSERT_EQ(result, 7);
 }
